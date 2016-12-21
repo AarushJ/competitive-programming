ad8bd7816d36240d12afb1df014ca412bba53ebc
AarushJ <avijuneja.cs@gmail.com>
2016-12-21 22:46:47 +0530

    added sieve of eratosthenes

---
+++ number theory/sieve of eratosthenes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+using namespace std;
+#define loop(i,a,b) for(int i = int(a); i<int(b) ; i++)
+#define MAX 10000000
+bool prime[MAX+1];			// due to global declaration , all elements will be set to false by default 
+							// prime[i] = false indicates prime 
+void sieve(){	   			// no need of memset because isPrime is global 
+	int root = sqrt(MAX);
+	loop(i,2,root+1){
+		if(!prime[i]){ 
+			for( int j = i*i ; j<=MAX ; j+=i ){
+				prime[j] = true; 
+			}	
+		}
+	}
+}
+
+int main(){
+	sieve();
+	loop(i,2,500){
+		if(!prime[i])
+			cout << i << " ";
+	}
+	return 0;
+}
