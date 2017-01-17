c6dad9dff2afa2b9dd22ba8b3c1abef5898e8a24
AarushJ <avijuneja.cs@gmail.com>
2017-01-18 00:55:56 +0530

    easy,adhoc

---
+++ Online judge solutions/Hackerrank/counting sort 1.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n ;
+    int num[n];
+    for(int i=0 ; i<n ; i++) cin >> num[i];
+    int count[100]={0};
+    for(int i=0 ; i<n ; i++){
+        count[num[i]]++;
+    }
+    for(int i=0 ; i<100 ; i++){
+        cout << count[i] << " ";
+    }
+    return 0;
+}
+
