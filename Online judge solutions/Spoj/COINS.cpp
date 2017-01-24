a7c90088d5c94ab4fb8110058cb68d5de5eef151
AarushJ <avijuneja.cs@gmail.com>
2017-01-25 00:32:53 +0530

    dynamic programming , top-down

---
+++ Online judge solutions/Spoj/COINS.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+typedef long long ll;
+
+map<ll,ll> maxCoins;
+
+ll coins(ll n){
+	if(maxCoins.find(n)!=maxCoins.end()){
+		return maxCoins[n];
+	}else{
+		maxCoins[n] = max(coins(n/2)+coins(n/3)+coins(n/4),n);
+	}
+	return maxCoins[n];
+}
+
+int main(){
+	ll n;
+	while(cin >> n){
+		maxCoins[0] = 0;
+		cout << coins(n) << "\n";
+	}
+	return 0;
+}
