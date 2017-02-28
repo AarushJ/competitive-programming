4da87644f8f27b17c5588de34c0511ce1e8101a7
AarushJ <avijuneja.cs@gmail.com>
2017-03-01 00:50:34 +0530

    simulation , 2 queues , good quest

---
+++ Online judge solutions/Codeforces/749C.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	char c;
+	queue<int> d,r;
+	for(int i=0 ; i<n ; i++){
+		cin >> c;
+		if(c=='D')	d.push(i);
+		else r.push(i);
+	}
+	while(!d.empty() && !r.empty()){
+		if(d.front() < r.front()){
+			r.pop();
+			d.push(d.front()+n);
+			d.pop();
+		}else{
+			d.pop();
+			r.push(r.front()+n);
+			r.pop();
+		}
+	}
+	if(d.empty()) cout << 'R';
+	else cout << 'D';
+	return 0;
+}
