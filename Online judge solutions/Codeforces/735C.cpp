2e46ffc4b0983b758f231c4c0cd352273491f13d
AarushJ <avijuneja.cs@gmail.com>
2017-03-01 19:45:34 +0530

    number theory,primes , goldbach's conjecture

---
+++ Online judge solutions/Codeforces/735C.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+bool isPrime(ll n){
+    if(n==2) return true;
+    if(n%2==0)  return false;
+    int root = sqrt(n);
+    for(int i=3 ; i<=root ; i+=2){
+        if(n%i==0)  return false;
+    }
+    return true;
+}
+
+int main(){
+    ll n;
+    cin >> n;
+    if(isPrime(n))  cout << 1;
+    else if(n%2==0)  cout << 2;
+    else if(isPrime(n-2))   cout << 2;
+    else cout << 3;
+    return 0;
+}
