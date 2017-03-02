55a84de6323667a711b6e39a3d70b5f1e4a43ef2
AarushJ <avijuneja.cs@gmail.com>
2017-03-02 21:28:44 +0530

    bitwise xor , observation

---
+++ Online judge solutions/Codeforces/742B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define lp(i,a,b) for(ll i = ll(a); i<ll(b) ; i++)
+
+
+int main(){
+	ll n,x,ans = 0;
+	cin >> n >> x;
+	map<ll,ll> m;
+	ll arr[n];
+	lp(i,0,n){
+		cin >> arr[i];
+		map<ll,ll>::iterator it = m.find(arr[i]^x);
+		if(it!=m.end()){
+			ans += it->second;
+		}
+		m[arr[i]] = m[arr[i]]+1;
+	}
+	cout << ans << "\n";	
+}
