6ce30c2a0ebe8da9c1707778016d89e6b0f457c5
AarushJ <avijuneja.cs@gmail.com>
2017-03-07 00:28:24 +0530

    edited union-find data structure

--- Data structures/union_find.cpp
+++ Data structures/union_find.cpp
@@ -3,37 +3,43 @@ using namespace std;
 
 typedef long long ll;
 
-#define lp(i,a,b)	for(ll i=ll(a) ; i<ll(b) ; i++)
+#define lp(i,a,b)	for(ll i=ll(a) ; i<=ll(b) ; i++)
 
-void init(int par[],int sz[],int n){
-	lp(i,0,n){
+#define N 100005
+ll n;
+ll par[N],sz[N];
+void init(){
+	lp(i,0,n-1){
 		par[i] = i;
 		sz[i] = 1;
 	}
 }
 
-int find(int par[],int i){
- 	while( i != par[i] )	i=par[par[i]];	// path compression
+ll find(ll i){
+ 	while( i != par[i] ){
+		par[i] = par[par[i]];	// path compression 
+		i = par[i];
+	} 	
 	return i;
 }
 
-void merge(int par[],int sz[],int u,int v){
-	int a = find(par,u);
-	int b = find(par,v);
+void merge(ll u,ll v){
+	ll a = find(u);
+	ll b = find(v);
 	if(a==b)	return; // union by rank/size
 	if( sz[a] < sz[b] ) { par[a] = b; sz[b]+=sz[a];	}
 	else				{ par[b] = a; sz[a]+=sz[b];	}
 }
 
-bool areConn(int u,int v,int par[]){
-	return (find(par,u)==find(par,v));
+bool areConn(ll u,ll v){
+	return (find(u)==find(v));
 }
 
 int main(){
-	int n;
-	int par[n],sz[n];
+	ll n;
+	
 	cin >> n;
-	init(par,sz,n);
+	init(n);
 	
 	
 	// corresponding merge, find and areConn functions...
