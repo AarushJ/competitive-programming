6c07bd201a2688c08b32271c21dbdc943a582d0c
AarushJ <avijuneja.cs@gmail.com>
2017-05-11 16:35:10 +0530

    number theory, fermat's sum of squares theorem

---
+++ Online judge solutions/Spoj/TWOSQRS.cpp
@@ -0,0 +1,127 @@
+/*
+	Fermat's sum of squares theorem has been used
+	
+	Theorem :- A number can be expressed as a sum of 2 squares if all of its prime factors which are of the form 4n+3 can occur even number of times in its 
+			   prime factorization.
+
+*/
+
+
+
+/*
+	AARUSH JUNEJA
+	@vivace
+	IIT(ISM),Dhanbad
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef double lf;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef set<ll> sll;
+typedef vector<ll> vll;
+typedef map<string,ll> msl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> ii;
+typedef vector<ii > vpll;
+typedef map<ll,ll> mll;
+typedef vector<vll > vvll;
+typedef list<ll> lll;
+typedef vector<lll > vlll;
+typedef pair<ll,ii> iii;
+
+#define rtn return 
+#define gc getchar
+#define pb push_back
+#define ff first
+#define ss second
+#define mp(x,y) make_pair(x,y)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+#define lp(i,b) for(ll i = ll(0); i<ll(b) ; i++) 
+#define lpit(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define mid(s,e) (s+(e-s)/2)
+#define min3(a,b,c)	min(a,min(b,c))
+#define max3(a,b,c)	max(a,max(b,c))
+
+ll ip(){
+	ll x = 0; bool isNeg = false; char c;
+	c = gc();
+	if(c == '-')	isNeg = true , c = gc();
+	for( ; c >= '0'  && c <= '9' ; c = gc()); x = (x << 1) + (x << 3) + c - '0';
+	if(isNeg)	x *= -1;	return x;
+}
+//////////////////////////////////////////// CONTAINERS FOR FUNCTIONS MENTIONED IN TEMPLATE ///////////////////////////////////
+#define MAX 100005
+//bool isPrime[MAX];
+//int spf[MAX];
+//////////////////////////////////////////// FUNCTIONS THAT NEED COMMENTED OUT CONTAINERS ////////////////////////////////////
+
+
+//void prime_sieve(){ for(ll i=2 ; i*i<=MAX ; i++){	if(!isPrime[i]){ for(ll j=i*i ; j<=MAX ; j+=i)	isPrime[j] = 1;} } 	}
+//void spf_sieve(){	for(ll i=2 ; i<MAX ; i+=2)	spf[i] = 2;
+//	for(ll i=3 ; i<MAX ; i+=2){	if(!isPrime[i]){ spf[i] = i; for(ll j=i ; (j*i)<MAX ; j+=2){ if(!isPrime[i*j])	isPrime[i*j] = true , spf[i*j] = i; } } }
+//}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+inline ll ncr(ll n,ll r){ ll ans = 1 ; if(r > n-r) r = n-r; lp(i,r+1){ans*=(n-i) ; ans/=(i+1); } rtn ans; }
+inline ll gcd(ll a,ll b){if(!a) rtn b; rtn gcd(b%a,a); }
+inline ll fme(ll x,ll n,ll mod){ll ans=1;x%=mod;while(n>0){if(n&1){ ans*=x;ans%=mod;} x*=x;x%=mod;n>>=1;}rtn ans%mod;}
+inline bool isPalin(string &s){	int len = s.size()-1;lp(i,(len/2)+2){if(!(s[i]==s[len-i]))	rtn false;} rtn true;}
+inline ll lcm(ll a,ll b){rtn (a*b)/gcd(a,b); }
+inline ll fmm(ll a,ll b,ll m) {ll r=0;a%=m;b%=m;while(b>0){if(b&1){r+=a;r%=m;}a+=a;a%=m;b>>=1;}rtn r%m;}
+inline ll sfme(ll a,ll b,ll m) {ll r=1;a%=m;while(b>0){if(b&1)r=fmm(r,a,m);a=fmm(a,a,m);b>>=1;}rtn r%m;}
+inline int in(){ int NR=0; register char c=gc(); while( c < 48 || c > 57 ){c=gc();} 
+	while(c>47 && c< 58){	NR = (NR << 3) + (NR << 1) + (c - 48); c=gc();} return NR;
+}
+
+ll dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
+//--------------------------------TEMPLATE ENDS HERE--------------------------------------------------//
+
+#define MOD 1000000007
+#define N 100010
+
+int main(){
+	ios::sync_with_stdio(false);
+	//cin.tie(NULL);
+	ll t,n,m,e,a,b,q,h,f,w,k,p;
+    ///////////// START FROM HERE ////////////////////
+	cin >> t;
+	
+	while(t--){
+		cin >> n;
+		if(!n)	cout << "Yes\n";
+		else{
+			bool flag = true;
+			for(ll i = 2 ; i*i <= n ; i++){
+				if( n % i == 0){
+					ll cnt = 0;
+					while( n % i == 0){
+						n/=i;
+						cnt++;
+					} 
+					
+					if( (i-3)%4 == 0 && cnt%2){
+						flag = false;
+						break;
+					}
+				}
+			}
+			// case : n is prime
+			if( n > 1 && (n-3)%4 == 0 )
+				flag = false;
+				
+			if( flag )	cout << "Yes\n";
+			else cout << "No\n";
+		
+		}
+		
+		
+	}
+	
+	
+	
+	return 0;
+}
