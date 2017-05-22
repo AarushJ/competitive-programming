c6f58595e3adac9ce4225d39f41839ee70a719d1
AarushJ <avijuneja.cs@gmail.com>
2017-05-22 22:25:47 +0530

    number theory, catalan numbers

---
+++ Online judge solutions/Codechef/DCE03.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+#define MAX 1000
+#define MOD 1000000
+ll cat[MAX+1];
+
+ll catalan(){
+	cat[0] = 1;
+	cat[1] = 1;
+	for(int i=2 ; i<=MAX ; i++){
+		cat[i] = 0;
+		for(int j=0 ; j<i ; j++){
+			cat[i] += (((cat[j]%MOD)*(cat[i-j-1]%MOD))%MOD);
+			cat[i]%=MOD;
+		}
+	}
+}
+
+int main(){
+	ll n;
+	catalan();
+        int t;
+        cin >> t;
+	while(t--){
+		cin >> n;
+                cout << cat[n] << "\n";
+	}
+	return 0;
+}
