16b0e9e11b2f8daa2c846dba3eac966cbc555d10
AarushJ <avijuneja.cs@gmail.com>
2017-06-19 15:57:32 +0530

    query on the DAG(Directed Acyclic Graph)  formed by DP(Dynamic Programming), RMQ, excellent question, too much to learn from this

---
+++ Online judge solutions/Codechef/SEAINCR.cpp
@@ -0,0 +1,155 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define N 100010
+
+typedef double lf;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef set<ll> sll;
+typedef vector<ll> vll;
+typedef map<string,ll> msl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> ii;
+typedef vector<ii > vpll;
+typedef map<ll,ll> mll;
+typedef vector<vll > vvll;
+typedef list<ll> lll;
+typedef vector<lll > vlll;
+typedef pair<ll,ii> iii;
+
+#define sd(a) scanf("%d",&a)
+#define sstr(a) scanf("%s",a)
+#define sl(a) scanf("%lld",&a)
+#define debug(a) printf("check%d\n",a)
+#define clr(a) memset(a,0,sizeof(a))
+#define rtn return
+#define gc getchar
+#define pb push_back
+#define ff first
+#define ss second
+#define mp(x,y) make_pair(x,y)
+#define all(a) a.begin(),a.end()
+#define allr(a) a.rbegin(),a.rend()
+#define lp(i,b) for(ll i = ll(0); i<ll(b) ; i++) 
+#define lpit(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define mid(s,e) (s+(e-s)/2)
+#define min3(a,b,c)	min(a,min(b,c))
+#define max3(a,b,c)	max(a,max(b,c))
+
+ll ip(){
+	ll x = 0; bool isNeg = false; char c;
+	c = gc();
+	if(c == '-')	isNeg = true , c = gc();
+	for( ; c >= '0'  && c <= '9' ; c = gc()); x = (x << 1) + (x << 3) + c - '0';
+	if(isNeg)	x *= -1;	return x;
+}
+//////////////////////////////////////////// CONTAINERS FOR FUNCTIONS MENTIONED IN TEMPLATE ///////////////////////////////////
+//#define MAX 100005
+//bool isPrime[MAX];
+//int spf[MAX];
+//////////////////////////////////////////// FUNCTIONS THAT NEED COMMENTED OUT CONTAINERS ////////////////////////////////////
+
+
+//void prime_sieve(){ for(ll i=2 ; i*i<=MAX ; i++){	if(!isPrime[i]){ for(ll j=i*i ; j<=MAX ; j+=i)	isPrime[j] = 1;} } 	}
+//void spf_sieve(){	for(ll i=2 ; i<MAX ; i+=2)	spf[i] = 2;
+//	for(ll i=3 ; i<MAX ; i+=2){	if(!isPrime[i]){ spf[i] = i; for(ll j=i ; (j*i)<MAX ; j+=2){ if(!isPrime[i*j])	isPrime[i*j] = true , spf[i*j] = i; } } }
+//}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+inline ll ncr(ll n,ll r){ ll ans = 1 ; if(r > n-r) r = n-r; lp(i,r+1){ans*=(n-i) ; ans/=(i+1); } rtn ans; }
+inline ll gcd(ll a,ll b){if(!a) rtn b; rtn gcd(b%a,a); }
+inline ll fme(ll x,ll n){ll ans=1;x%=MOD;while(n>0){if(n&1){ ans*=x;ans%=MOD;} x*=x;x%=MOD;n>>=1;}rtn ans%MOD;}
+inline bool isPalin(string &s){	int len = s.size()-1;lp(i,(len/2)+2){if(!(s[i]==s[len-i]))	rtn false;} rtn true;}
+inline ll lcm(ll a,ll b){rtn (a*b)/gcd(a,b); }
+inline ll fmm(ll a,ll b) {ll r=0;a%=MOD;b%=MOD;while(b>0){if(b&1){r+=a;r%=MOD;}a+=a;a%=MOD;b>>=1;}rtn r%MOD;}
+inline ll sfme(ll a,ll b) {ll r=1;a%=MOD;while(b>0){if(b&1)r=fmm(r,a);a=fmm(a,a);b>>=1;}rtn r%MOD;}
+inline int in(){ int NR=0; register char c=gc(); while( c < 48 || c > 57 ){c=gc();} 
+	while(c>47 && c< 58){	NR = (NR << 3) + (NR << 1) + (c - 48); c=gc();} return NR;
+}
+
+ll dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
+//--------------------------------TEMPLATE ENDS HERE--------------------------------------------------//
+
+#define MAX 1000005
+ll bit[MAX], lastIndex[MAX], maxx[MAX], arr[MAX], ans[MAX], n; 
+vector<pair<ll,ll> > Q[MAX]; 	// ( Q[i] : queries beginning from i . pair<ll,ll> => (end index of query, query number) )
+vll g[MAX]; // graph (DAG of LIS)
+
+/* maxx[j] => lenght of LIS ending at index j. We start discovering the indexes from right end of array .
+ So if we have reached index i, s.t i < j, then maxx[j] will need to be updated (its initially
+ zero when i > j, 1 when i == j and changes accordingly when i < j) */
+
+/* DETAILED EXPLANATION IN NOTEBOOK */
+
+ll query(ll i){
+	ll ans = 0;
+	while( i > 0 ){
+		ans = max(ans, bit[i]);
+		i -= (i & -i);
+	}
+	return ans;
+}
+	
+void update(ll i, ll val){
+	while( i <= n ){
+		bit[i] = max( bit[i], val);
+		i +=(i & -i);
+	}
+}
+
+// DFS style
+void updateDAG(ll currIndex, ll lenOfLIS){
+	if( lenOfLIS > maxx[currIndex] ){
+		for(ll i = 0 ; i<g[currIndex].size() ; i++){
+			updateDAG( g[currIndex][i], lenOfLIS + 1); 
+		}
+		maxx[currIndex] = lenOfLIS;
+		update( currIndex, lenOfLIS);
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll t, q;
+	sl(t);
+	while(t--){
+		sl(n); sl(q);
+		lp(i, n+1){
+			bit[i] = lastIndex[i] = maxx[i] = 0;
+			Q[i].clear();
+			g[i].clear();
+		}
+
+		for(int i = 1 ; i<=n ; i++){
+			sl(arr[i]);
+			lastIndex[arr[i]] = i;
+			int indexOfNextSmallest = 0;	// next smallest to arr[i] which is already taken as input and thus has index < i
+			for(int j = arr[i] - 1 ; j>=1 ; j--){
+				if( lastIndex[j] > indexOfNextSmallest ){
+					indexOfNextSmallest = lastIndex[j];
+					g[lastIndex[j]].pb(i);
+				}
+			}
+		}
+		
+		lp(i, q){
+			ll x, y;
+			sl(x); sl(y);
+			Q[x].pb(mp(y, i));
+		}
+
+		for(int i = n ; i>=1 ; i--){
+			updateDAG(i, 1);
+			lpit(it, Q[i]){
+				ans[it->ss] = query(it->ff);
+			}
+		}
+		lp(i,q)	printf("%lld\n", ans[i]);
+	}
+		
+
+	return 0;
+}
