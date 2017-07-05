84e4fe000ca86e7228a532f5a91e5d7f952fb18b
AarushJ <avijuneja.cs@gmail.com>
2017-07-05 17:54:41 +0530

    removed a minor bug

--- Data structures/Union_find.cpp
+++ Data structures/Union_find.cpp
@@ -1,17 +1,23 @@
-#include<bits/stdc++.h>
-using namespace std;
+/* 
+	Disjoint set union 
+
+	=> 0-indexed
+	=> implemented path compression and union by rank
+	=> O(logn) per mergeit/find/areConnected   functions
+*/
 
 typedef long long ll;
 
-#define lp(i,a,b)	for(ll i=ll(a) ; i<=ll(b) ; i++)
+#define lp(i,b) for(ll i = ll(0); i<ll(b) ; i++) 
+#define NMAX 100005
 
-#define N 100005
 ll n;
-ll par[N],sz[N];
-void init(){
-	lp(i,0,n-1){
+ll par[NMAX], rank[NMAX];
+
+void init(ll n){
+	lp(i,n){
 		par[i] = i;
-		sz[i] = 1;
+		rank[i] = 0;
 	}
 }
 
@@ -23,27 +29,16 @@ ll find(ll i){
 	return i;
 }
 
-void merge(ll u,ll v){
+void mergeit(ll u,ll v){
 	ll a = find(u);
 	ll b = find(v);
 	if(a==b)	return; // union by rank/size
-	if( sz[a] < sz[b] ) { par[a] = b; sz[b]+=sz[a];	}
-	else				{ par[b] = a; sz[a]+=sz[b];	}
+	if( rank[a] < rank[b] ) swap(a, b);
+	else if( rank[a] == rank[b] )	++rank[a];
+	par[b] = a;
 }
 
-bool areConn(ll u,ll v){
+bool areConnected(ll u,ll v){
 	return (find(u)==find(v));
 }
 
-int main(){
-	ll n;
-	
-	cin >> n;
-	init(n);
-	
-	
-	// corresponding merge, find and areConn functions...
-	
-	
-	return 0;
-}
